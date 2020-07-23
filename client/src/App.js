68406a231e4bba5f7eac16aeddd46f9897884c6a
siddhantchopra <siddanthchopra@gmail.com>
2020-07-23 17:57:31 +0530

    Changed heading

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { FileUpload } from './components/FileUpload';
 function App() {
   return (<div className="container mt-4">
     <h4 className="display-4 text-center mb-4">
-      <i className="fab fa-react"></i> React File Upload
+      <i className="fab fa-react"></i> React Image Upload
     </h4>
     <FileUpload/>
   </div>
